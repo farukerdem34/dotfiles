0a5a5e93da8f8b797d04ebe5c654b460786708bf
Faruk <farukomererdem2003@gmail.com>
2025-02-17 00:49:03 +0300

    tmux added

--- init-server.sh
+++ init-server.sh
@@ -2,10 +2,19 @@
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y gcc make nvim stow
+sudo apt install -y gcc make nvim stow tmux
+
 git clone https://github.com/farukerdem34/dotfiles $HOME/.dotfiles
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 cd $HOME/.dotfiles/
 mv -u $HOME/.bashrc .bashrc.bak
-stow bash nvim
+mv -u $HOME/.tmux.conf .tmux.conf.bak
+rm -rf $HOME/.config/nvim
+
+stow bash nvim tmux
+
 cd $HOME
+~/.tmux/plugins/tpm/bin/install_plugins
+nvim --headless "+Lazy! sync" +qa
 . .bashrc

493467608e0dc8a389c7b67d2be2880342c91368
Faruk <farukomererdem2003@gmail.com>
2025-02-23 16:40:45 +0300

    alias and completion files

---
+++ zsh/.zsh_aliases.zsh
@@ -0,0 +1,73 @@
+# Aliases
+if command -v eza &> /dev/null;
+then
+    alias ll='eza -lh --icons=always --color=always'
+    alias ls='eza --icons=always --color=always'
+else
+    alias ls='ls --color'
+    alias ll='ls -lh --color'
+fi
+alias nano='vim'
+alias vi='vim'
+
+if command -v docker &> /dev/null;
+then
+    source <(docker completion zsh)
+fi
+
+if  command -v gh &> /dev/null;
+then 
+    source <(gh completion -s zsh)
+fi
+
+if [[ $TERM == "xterm-kitty" ]] ;
+then 
+    alias ssh="kitty +kitten ssh"
+fi
+if  command -v yay &> /dev/null;
+then 
+    alias yayy='yay --noconfirm --color always '
+fi
+
+if command -v zoxide &> /dev/null;
+then
+  eval "$(zoxide init --cmd cd zsh)"
+fi
+
+
+if command -v nvim &> /dev/null; then
+  export EDITOR=/usr/bin/nvim
+  alias vim=nvim
+fi
+
+if  command -v wg &> /dev/null ; then
+  alias wg='sudo wg'
+  alias wg-quick='sudo wg-quick'
+fi
+
+if  command -v fastfetch &> /dev/null ; then
+alias ff='fastfetch -c $HOME/.config/fastfetch/config.jsonc'
+fi
+
+alias copilot='gh copilot explain '
+if  command -v bat &> /dev/null ; then
+  alias bat='bat --theme="Catppuccin Macchiato" --color=always -p '
+  alias cat=bat
+fi
+if  command -v fzf &> /dev/null ; then
+  alias fzf='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+  alias vimzf='vim $(fzf)'
+fi
+
+if command -v lazygit &> /dev/null; then
+  alias lzg="lazygit"
+fi
+
+if command -v lazydocker &> /dev/null; then
+  alias lzd="lazydocker"
+fi
+
+alias dc='docker compose'
+export PATH=$PATH:$HOME/.local/bin/
+alias wg='sudo wg'
+alias wg-quick='sudo wg-quick'

29165a19735311f31c67ce59fbd5a9c8c55fbf5e
Faruk <farukomererdem2003@gmail.com>
2025-02-23 18:14:05 +0300

    init server script

--- init-server.sh
+++ init-server.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
-
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y gcc make neovim stow tmux
+packages='gcc make neovim stow tmux git curl'
+if command -v sudo &>/dev/null; then
+  sudo apt update -y
+  sudo apt upgrade -y
+  sudo apt install -y "$packages"
+else
+  apt update -y
+  apt upgrade -y
+  apt install -y "$packages"
+fi
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-cd $HOME/.dotfiles/
+cd "$HOME"/.dotfiles/ || git clone https://github.com/farukerdem34/dotfiles.git "$HOME"/.dotfiles
+cd "$HOME"/.dotfiles || exit
 
-if [[ -e $HOME/.bashrc ]]; then
-  mv -u $HOME/.bashrc .bashrc.bak
+if [[ -e "$HOME"/.bashrc ]]; then
+  mv -u "$HOME"/.bashrc .bashrc.bak
 fi
 
-if [[ -e $HOME/.tmux.conf ]]; then
-  mv -u $HOME/.tmux.conf .tmux.conf.bak
+if [[ -e "$HOME"/.tmux.conf ]]; then
+  mv -u "$HOME"/.tmux.conf .tmux.conf.bak
 fi
 
-if [[ -e $HOME/.config/nvim ]]; then
-  rm -rf $HOME/.config/nvim
+if [[ -e "$HOME"/.config/nvim ]]; then
+  rm -rf "$HOME"/.config/nvim
 fi
 
 stow bash nvim tmux
 
-cd $HOME
+cd "$HOME" || exit
 ~/.tmux/plugins/tpm/bin/install_plugins
+
 nvim --headless "+Lazy! sync" +qa
-cd $HOME
-. .bashrc
+
+cd "$HOME" || exit
+
+yes y | curl -sS https://starship.rs/install.sh | sh
+
+if [[ -f "$HOME"/.bashrc ]]; then
+  . .bashrc
+fi

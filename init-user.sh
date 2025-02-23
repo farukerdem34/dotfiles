5eddd30a8ec13ba5e95063f7bcd76171cdef0318
Faruk <farukomererdem2003@gmail.com>
2025-02-23 18:41:49 +0300

    new packages, backup optimizations

--- init-user.sh
+++ init-user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-packages="stow neovim tmux bat fzf git gcc btop make fastfetch kitty zsh starship"
+packages="stow neovim tmux bat fzf git gcc btop make fastfetch kitty zsh starship lazygit lazydocker"
 if command -v yay &>/dev/null; then
   yay -Syu $packages
 elif command -v pacman &>/dev/null; then
@@ -9,102 +9,78 @@ elif command -v apt &>/dev/null; then
   sudo apt install -y $packages
 fi
 
-git clone https://github.com/tmux-plugins/tpm "$HOME"/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-cd "
-
-"/.dotfiles/
-
+if [[ -e ~/.dotfiles ]]; then
+  cd ~/.dotfiles/ || exit
+else
+  git clone https://github.com/farukerdem34/dotfiles ~/.dotfiles
+  cd ~/.dotfiles || exit
+fi
 # Backup .bashrc
-if [[ -e "
+if [[ -e ~/.bashrc ]]; then
+  mv -u ~/.bashrc .bashrc.bak
+fi
 
-"/.bashrc ]]; then
-  mv -u "
+if [[ -e ~/.bash_aliases ]]; then
+  mv ~/.bash_aliases ~/.bash_aliases.bak
+fi
 
-"/.bashrc .bashrc.bak
+if [[ -e ~/.bash_completions ]]; then
+  mv ~/.bash_completions ~/.bash_completions.bak
 fi
 
 # Backup .tmux.conf
-if [[ -e "
-
-"/.tmux.conf ]]; then
-  mv -u "
-
-"/.tmux.conf .tmux.conf.bak
+if [[ -e ~/.tmux.conf ]]; then
+  mv -u ~/.tmux.conf .tmux.conf.bak
 fi
 
 # Backup NeoVim Config
-if [[ -d "
-
-"/.config/nvim ]]; then
-  tar -zcvf "
-
-"/.config/nvim.bak "
-
-"/.config/nvim
+if [[ -d ~/.config/nvim ]]; then
+  tar -zcvf ~/.config/nvim.bak ~/.config/nvim
 fi
 
 # Backup Kitty Config
-if [[ -d "
-
-"/.config/kitty ]]; then
-  tar -zcvf "
-
-"/.config/kitty.bak "
-
-"/.config/kitty
+if [[ -d ~/.config/kitty ]]; then
+  tar -zcvf ~/.config/kitty.bak ~/.config/kitty
 fi
 
 # Backup fastfetch
-if [[ -d "
-
-"/.config/fastfetch ]]; then
-  tar -zcvf "
-
-"/.config/fastfetch.bak "
-
-"/.config/fastfetch
+if [[ -d ~/.config/fastfetch ]]; then
+  tar -zcvf ~/.config/fastfetch.bak ~/.config/fastfetch
 fi
 
-if [[ -d "
-
-"/.config/bat ]]; then
-  tar -zcvf "
-
-"/.config/bat.bak "
-
-"/.config/bat
+# Backup bat
+if [[ -d ~/.config/bat ]]; then
+  tar -zcvf ~/.config/bat.bak ~/.config/bat
 fi
 
-if [[ -d "
-
-"/.config/btop ]]; then
-  tar -zcvf "
-
-"/.config/btop.bak "
-
-"/.config/btop
+# Backup btop
+if [[ -d ~/.config/btop ]]; then
+  tar -zcvf ~/.config/btop.bak ~/.config/btop
 fi
 
 # Backup zsh
-if [[ -e "
-
-"/.zshrc ]]; then
-  cp "
-
-"/.zshrc "
-
-"/.zshrc.bak
+if [[ -e ~/.zshrc ]]; then
+  mv ~/.zshrc ~/.zshrc.bak
 fi
 
-stow bash bat fastfetch kitty nvim btop tmux vimrc zsh starship
+if [[ -e ~/.zsh_aliases.zsh ]]; then
+  mv ~/.zsh_alieses ~/.zsh_alieses.bak
+fi
 
-cd "
+if [[ -e ~/.zsh_completion_sources.zsh ]]; then
+  mv ~/.zsh_completion_sources.zsh ~/.zsh_completion_sources.zsh.bak
+fi
 
-"
+stow neovim tmux bat btop fastfetch kitty zsh starship
+
 ~/.tmux/plugins/tpm/bin/install_plugins
-nvim --headless "+Lazy! sync" +qa
-cd "
 
-"
-. .bashrc
+nvim --headless "+Lazy! sync" +qa
+
+if [[ -z $BASH_VERSION ]]; then
+  . ~/.bashrc
+elif [[ -z $ZSH_VERSION ]]; then
+  . ~/.zshrc
+fi

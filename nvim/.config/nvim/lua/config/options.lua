8106023d6fd1540c4ea70eb48fb9c9aff99f27f0
Faruk <farukomererdem2003@gmail.com>
2025-03-01 22:19:06 +0300

    rust-analyzer mason

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,9 @@
 -- Add any additional options here
 --
 vim.g.mapleader = " "
+
+-- LSP Server to use for Rust.
+-- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
+-- only for diagnostics. The rest of LSP support will still be
+-- provided by rust-analyzer.
+vim.g.lazyvim_rust_diagnostics = "rust-analyzer"

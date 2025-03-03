b98d04d90c8985a8974399f1e6db91a0f988f170
Faruk <farukomererdem2003@gmail.com>
2025-03-03 11:39:33 +0300

    eza

--- init-user.sh
+++ init-user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-packages="stow neovim tmux bat fzf git gcc btop make fastfetch kitty zsh starship lazygit lazydocker zoxide"
+packages="stow neovim tmux bat fzf git gcc btop make fastfetch kitty zsh starship lazygit lazydocker zoxide eza"
 if command -v yay &>/dev/null; then
   yay -Syu $packages
 elif command -v pacman &>/dev/null; then

773e7308e2a0723c94abce309a4efb62590fcee6
Faruk <farukomererdem2003@gmail.com>
2025-03-16 17:00:41 +0300

    fancy update

--- init-user.sh
+++ init-user.sh
@@ -1,86 +1,140 @@
 #!/bin/bash
-packages="stow neovim tmux bat fzf git gcc btop make fastfetch kitty zsh starship lazygit lazydocker zoxide eza"
-if command -v yay &>/dev/null; then
-  yay -Syu $packages
-elif command -v pacman &>/dev/null; then
-  sudo pacman -Syu $packages
-elif command -v apt-get &>/dev/null; then
-  sudo apt-get update -y
-  sudo apt-get install -y $packages
-fi
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+print_muave() {
+  echo -e "\e[38;2;203;166;247m$1\e[0m"
+}
+print_red() {
+  echo -e "\e[38;2;243;139;168m$1\e[0m"
+  exit
+}
 
-if [[ -e ~/.dotfiles ]]; then
-  cd ~/.dotfiles/ || exit
-else
-  git clone https://github.com/farukerdem34/dotfiles ~/.dotfiles
-  cd ~/.dotfiles || exit
-fi
-# Backup .bashrc
-if [[ -e ~/.bashrc ]]; then
-  mv -u ~/.bashrc .bashrc.bak
-fi
+print_green() {
+  echo -e "\e[38;2;166;227;161m$1\e[0m"
+}
 
-if [[ -e ~/.bash_aliases ]]; then
-  mv ~/.bash_aliases ~/.bash_aliases.bak
-fi
+install_packages() {
+  print_muave "Installing packages"
+  packages="stow neovim tmux bat fzf git gcc btop make fastfetch kitty zsh starship lazygit lazydocker zoxide eza"
+  if command -v yay &>/dev/null; then
+    yay -Syu $packages
+  elif command -v pacman &>/dev/null; then
+    sudo pacman -Syu $packages
+    # elif command -v apt-get &>/dev/null; then
+    #   sudo apt-get update -y
+    #   sudo apt-get install -y $packages
+  else
+    print_red "No valid package manager found"
+    exit 1
+  fi
+  print_green "Packages installed"
+}
 
-if [[ -e ~/.bash_completions ]]; then
-  mv ~/.bash_completions ~/.bash_completions.bak
-fi
+install_tpm() {
+  print_muave "Cloning TPM"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm -q
+  print_muave "Installing TPM plugins"
+  ~/.tmux/plugins/tpm/bin/install_plugins >/dev/null 2>&1
+  print_green "TPM installed successfully"
+}
 
-# Backup .tmux.conf
-if [[ -e ~/.tmux.conf ]]; then
-  mv -u ~/.tmux.conf .tmux.conf.bak
-fi
+clone_repo() {
+  if [[ -d "$HOME"/.dotfiles ]]; then
+    print_muave "$HOME/.dotfiles already exists"
+  else
+    print_muave "Cloning dotfiles"
+    git clone https://github.com/farukerdem34/dotfiles.git $HOME/.dotfiles -q
+    print_green "Dotfiles cloned successfully"
+  fi
+}
 
-# Backup NeoVim Config
-if [[ -d ~/.config/nvim ]]; then
-  tar -zcvf ~/.config/nvim.bak ~/.config/nvim
-fi
+backup() {
+  # Backup .bashrc
+  if [[ -e ~/.bashrc ]]; then
+    print_muave "Backing up .bashrc"
+    mv -u ~/.bashrc .bashrc.bak
+  fi
 
-# Backup Kitty Config
-if [[ -d ~/.config/kitty ]]; then
-  tar -zcvf ~/.config/kitty.bak ~/.config/kitty
-fi
+  if [[ -e ~/.bash_aliases ]]; then
+    print_muave "Backing up .bash_aliases"
+    mv ~/.bash_aliases ~/.bash_aliases.bak
+  fi
 
-# Backup fastfetch
-if [[ -d ~/.config/fastfetch ]]; then
-  tar -zcvf ~/.config/fastfetch.bak ~/.config/fastfetch
-fi
+  if [[ -e ~/.bash_completions ]]; then
+    print_muave "Backing up .bash_completions"
+    mv ~/.bash_completions ~/.bash_completions.bak
+  fi
 
-# Backup bat
-if [[ -d ~/.config/bat ]]; then
-  tar -zcvf ~/.config/bat.bak ~/.config/bat
-fi
+  # Backup .tmux.conf
+  if [[ -e ~/.tmux.conf ]]; then
+    print_muave "Backing up .tmux.conf"
+    mv -u ~/.tmux.conf .tmux.conf.bak
+  fi
 
-# Backup btop
-if [[ -d ~/.config/btop ]]; then
-  tar -zcvf ~/.config/btop.bak ~/.config/btop
-fi
+  # Backup NeoVim Config
+  if [[ -d ~/.config/nvim ]]; then
+    print_muave "Backing up nvim"
+    tar -zcvf ~/.config/nvim.bak ~/.config/nvim
+    rm -rf ~/.config/nvim
+  fi
 
-# Backup zsh
-if [[ -e ~/.zshrc ]]; then
-  mv ~/.zshrc ~/.zshrc.bak
-fi
+  # Backup Kitty Config
+  if [[ -d ~/.config/kitty ]]; then
+    print_muave "Backing up kitty"
+    tar -zcvf ~/.config/kitty.bak ~/.config/kitty
+    rm -rf ~/.config/kitty
+  fi
 
-if [[ -e ~/.zsh_aliases.zsh ]]; then
-  mv ~/.zsh_alieses ~/.zsh_alieses.bak
-fi
+  # Backup fastfetch
+  if [[ -d ~/.config/fastfetch ]]; then
+    print_muave "Backing up fastfetch"
+    tar -zcvf ~/.config/fastfetch.bak ~/.config/fastfetch
+    rm -rf ~/.config/fastfetch
+  fi
 
-if [[ -e ~/.zsh_completion_sources.zsh ]]; then
-  mv ~/.zsh_completion_sources.zsh ~/.zsh_completion_sources.zsh.bak
-fi
+  # Backup bat
+  if [[ -d ~/.config/bat ]]; then
+    print_muave "Backing up bat"
+    tar -zcvf ~/.config/bat.bak ~/.config/bat
+    rm -rf ~/.config/bat
+  fi
 
-stow nvim tmux bat btop fastfetch kitty zsh starship
+  # Backup btop
+  if [[ -d ~/.config/btop ]]; then
+    muaave "Backing up btop"
+    tar -zcvf ~/.config/btop.bak ~/.config/btop
+    rm -rf ~/.config/btop
+  fi
 
-~/.tmux/plugins/tpm/bin/install_plugins
+  # Backup zsh
+  if [[ -e ~/.zshrc ]]; then
+    print_muave "Backing up zsh"
+    mv ~/.zshrc ~/.zshrc.bak
+  fi
 
-nvim --headless "+Lazy! sync" +qa
+  if [[ -e ~/.zsh_aliases.zsh ]]; then
+    print_muave "Backing up zsh_aliases"
+    mv ~/.zsh_alieses ~/.zsh_alieses.bak
+  fi
 
-if [[ -z $BASH_VERSION ]]; then
-  . ~/.bashrc
-elif [[ -z $ZSH_VERSION ]]; then
-  . ~/.zshrc
-fi
+  if [[ -e ~/.zsh_completion_sources.zsh ]]; then
+    print_muave "Backing up zsh_completion_sources"
+    mv ~/.zsh_completion_sources.zsh ~/.zsh_completion_sources.zsh.bak
+  fi
+  print_green "Backups created successfully"
+}
+
+stow_packages() {
+  cd ~/.dotfiles || print_red "Failed to change directory to $HOME/.dotfiles"
+  print_muave "Stowing packages"
+  stow nvim tmux bat btop fastfetch kitty zsh starship
+  print_green "Packages stowed successfully"
+}
+
+install_lazyvim() {
+  print_muave "Installing LazyVim"
+  nvim --headless "+Lazy! sync" +qa
+  print_green "LazyVim installed successfully"
+}
+
+print_muave "Dotfiles installed successfully"
+tmux

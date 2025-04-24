adfd1343a0cb9e9fd11d329f4e5ca4bb1edf16dd
Faruk ERDEM <farukomererdem2003@gmail.com>
2025-04-24 19:52:17 +0300

    new packages

--- init-user.sh
+++ init-user.sh
@@ -23,7 +23,7 @@ install_hyprland() {
 
 install_packages() {
   print_muave "Installing packages"
-  packages="stow neovim tmux bat fzf git gcc btop make fastfetch kitty zsh starship lazygit lazydocker zoxide eza timeshift-autosnap grub-btrfsd inotify-tools timeshift"
+  packages="stow cliphist neovim tmux bat fzf git gcc btop make fastfetch kitty zsh starship lazygit lazydocker zoxide eza timeshift-autosnap grub-btrfsd inotify-tools timeshift hyprpicker"
   if command -v yay &>/dev/null; then
     yayy $packages
   elif command -v pacman &>/dev/null; then

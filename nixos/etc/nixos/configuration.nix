6ef5062b777eb4e136b2a7a13ed27a0dc57b1b99
Faruk <farukomererdem2003@gmail.com>
2025-08-01 17:08:29 +0300

    container tools & firewall

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -175,6 +175,8 @@
   virt-manager
   lazygit
   # lazydocker
+  podman-compose
+  buildah
   hcloud
   ansible
   terraform
@@ -205,7 +207,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-#   networking.firewall.enable = true;
+  networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions

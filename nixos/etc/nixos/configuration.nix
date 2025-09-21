c669f4db19aa7e6cd35e72b677dc2844f6b41c91
Faruk <farukomererdem2003@gmail.com>
2025-09-21 12:48:25 +0300

    gdm to ly

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -65,7 +65,8 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.displayManager.gdm.enable = true;
+  services.displayManager.ly.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   # Hyprland
   programs.hyprland.enable = true;

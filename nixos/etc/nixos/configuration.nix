b7dc9ea1c605a85b7febe0977e8fd45a3dfc6505
Faruk <farukomererdem2003@gmail.com>
2025-10-09 20:36:32 +0300

    pantheon

--- nixos/etc/nixos/configuration.nix
+++ nixos/etc/nixos/configuration.nix
@@ -61,8 +61,8 @@
 
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
-  services.displayManager.ly.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.pantheon.enable = true;
   services.hardware.bolt.enable = true;
   # Configure keymap in X11
   services.xserver.xkb = {
